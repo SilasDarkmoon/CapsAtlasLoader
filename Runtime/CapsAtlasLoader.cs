067d2177c270b727b30f92ffb580324bdc306f4d
Silas <hdss3609@gmail.com>
2020-01-14 23:07:05 +0800

    理论上讲，atlas和sprite之间的引用关系是自动正确维护的

--- Runtime/CapsAtlasLoader.cs
+++ Runtime/CapsAtlasLoader.cs
@@ -16,76 +16,59 @@ namespace Capstones.UnityEngineEx
         // In current version of UGUI, the Image.RebuildImage will crash when the image's active sprite is set to null before the atlas is loaded.
         // So we get this list using reflection, and do the filter by ourselves.
         private static List<Image> TrackedImages;
+        // We find that, sometimes, atlas will be unloaded while it is actually in use.
+        // This may be because that, we are not holding a reference in the C#.
+        // This field make a map from WeakReference<Sprite> to Atlas, so we hold a reference to atlas, and can release the atlas's reference when the sprite is unloaded.
+        // TODO: common lifetime holder.
+        private static List<Pack<WeakReference, SpriteAtlas, string>> _LifetimeHolder = new List<Pack<WeakReference, SpriteAtlas, string>>();
 
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         private static void OnUnityStart()
         {
             TrackedImages = typeof(Image).GetField("m_TrackedTexturelessImages", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic).GetValue(null) as List<Image>;
+            UnityEngineEx.ResManager.GarbageCollector.GarbageCollectorEvents[1] += UnloadUnusedAtlas;
 
             if (ResManager.ResLoader is ResManager.ClientResLoader)
             {
-                SpriteAtlasManager.atlasRequested += (name, funcReg) =>
-                {
-                    var atlas = ResManager.LoadRes("atlas/" + name, typeof(SpriteAtlas)) as SpriteAtlas;
-                    if (atlas)
-                    {
-                        var sprites = Resources.FindObjectsOfTypeAll<Sprite>();
-                        for (int i = 0; i < sprites.Length; ++i)
-                        {
-                            var sprite = sprites[i];
-                            if (!sprite.texture)
-                            {
-                                if (atlas.CanBindTo(sprite))
-                                {
-                                    _LifetimeHolder.Add(new Pack<WeakReference, SpriteAtlas, string>(new WeakReference(sprite), atlas, sprite.name));
-                                }
-                            }
-                        }
-
-                        if (!_LifetimeChecking)
-                        {
-                            CoroutineRunner.StartCoroutine(CheckLifetimeWork());
-                        }
-
-                        for (var i = TrackedImages.Count - 1; i >= 0; --i)
-                        {
-                            var g = TrackedImages[i];
-                            if (!(g.overrideSprite != null ? g.overrideSprite : g.sprite))
-                            {
-                                TrackedImages.RemoveAt(i);
-                            }
-                        }
-                        funcReg(atlas);
-                    }
-                };
+                // TODO: when we change the ResLoader dynamically (from EditorResLoader to ClientResLoader), we should call this again.
+                // or (from ClientResLoader to EditorResLoader) we should unregister SpriteAtlasManager.atlasRequested.
+                SpriteAtlasManager.atlasRequested += LoadAtlas;
             }
         }
 
-        private static bool _LifetimeChecking = false;
-        private static List<Pack<WeakReference, SpriteAtlas, string>> _LifetimeHolder = new List<Pack<WeakReference, SpriteAtlas, string>>();
-        private static IEnumerator CheckLifetimeWork()
+        public static void LoadAtlas(string name, Action<SpriteAtlas> funcReg)
         {
-            _LifetimeChecking = true;
-            try
+            var atlas = ResManager.LoadRes("atlas/" + name, typeof(SpriteAtlas)) as SpriteAtlas;
+            if (atlas)
             {
-                while (true)
+                for (var i = TrackedImages.Count - 1; i >= 0; --i)
                 {
-                    for (var i = _LifetimeHolder.Count - 1; i >= 0; --i)
+                    var g = TrackedImages[i];
+                    var sprite = g.overrideSprite != null ? g.overrideSprite : g.sprite;
+                    if (!sprite)
                     {
-                        var item = _LifetimeHolder[i];
-                        if (!item.t1.GetWeakReference<Texture>())
-                        {
-                            PlatDependant.LogError($"Atlas of Sprite {item.t3} need to be unloaded.");
-                            _LifetimeHolder.RemoveAt(i);
-                        }
+                        TrackedImages.RemoveAt(i);
                     }
-                    yield return null;
+                    //else if (atlas.CanBindTo(sprite))
+                    //{
+                    //    _LifetimeHolder.Add(new Pack<WeakReference, SpriteAtlas, string>(new WeakReference(sprite), atlas, sprite.name));
+                    //}
+                }
+                funcReg(atlas);
+            }
+        }
+
+        public static IEnumerator UnloadUnusedAtlas()
+        {
+            for (int i = _LifetimeHolder.Count - 1; i >= 0; --i)
+            {
+                var info = _LifetimeHolder[i];
+                if (!info.t1.GetWeakReference<Sprite>())
+                {
+                    _LifetimeHolder.RemoveAt(i);
                 }
             }
-            finally
-            {
-                _LifetimeChecking = false;
-            }
+            yield return Resources.UnloadUnusedAssets();
         }
     }
 }
