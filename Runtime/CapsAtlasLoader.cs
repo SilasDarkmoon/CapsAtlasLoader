47b13f3710a9b82d1ef5122d54d1fbc03584f412
Silas <hdss3609@gmail.com>
2020-01-15 09:27:10 +0800

    暂时不去卸载atlas

--- Runtime/CapsAtlasLoader.cs
+++ Runtime/CapsAtlasLoader.cs
@@ -17,22 +17,23 @@ namespace Capstones.UnityEngineEx
         // So we get this list using reflection, and do the filter by ourselves.
         private static List<Image> TrackedImages;
         // We find that, sometimes, atlas will be unloaded while it is actually in use.
-        // This may be because that, we are not holding a reference in the C#.
-        // This field make a map from WeakReference<Sprite> to Atlas, so we hold a reference to atlas, and can release the atlas's reference when the sprite is unloaded.
+        // This may be because that, an auto garbage collect is triggered after the scene is loaded.
+        // while the scene objs are going to refer atlas, but the refer has not been made, as the garbage collect happens too early.
         // TODO: common lifetime holder.
-        private static List<Pack<WeakReference, SpriteAtlas, string>> _LifetimeHolder = new List<Pack<WeakReference, SpriteAtlas, string>>();
+        private static List<SpriteAtlas> _LifetimeHolder = new List<SpriteAtlas>();
 
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         private static void OnUnityStart()
         {
             TrackedImages = typeof(Image).GetField("m_TrackedTexturelessImages", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic).GetValue(null) as List<Image>;
-            UnityEngineEx.ResManager.GarbageCollector.GarbageCollectorEvents[1] += UnloadUnusedAtlas;
 
             if (ResManager.ResLoader is ResManager.ClientResLoader)
             {
                 // TODO: when we change the ResLoader dynamically (from EditorResLoader to ClientResLoader), we should call this again.
                 // or (from ClientResLoader to EditorResLoader) we should unregister SpriteAtlasManager.atlasRequested.
                 SpriteAtlasManager.atlasRequested += LoadAtlas;
+
+                UnityEngine.SceneManagement.SceneManager.sceneLoaded += (scene, mode) => CheckAtlasLifetime();
             }
         }
 
@@ -49,26 +50,50 @@ namespace Capstones.UnityEngineEx
                     {
                         TrackedImages.RemoveAt(i);
                     }
-                    //else if (atlas.CanBindTo(sprite))
-                    //{
-                    //    _LifetimeHolder.Add(new Pack<WeakReference, SpriteAtlas, string>(new WeakReference(sprite), atlas, sprite.name));
-                    //}
                 }
+                _LifetimeHolder.Add(atlas);
                 funcReg(atlas);
             }
         }
 
-        public static IEnumerator UnloadUnusedAtlas()
+        private static void CheckAtlasLifetime()
         {
-            for (int i = _LifetimeHolder.Count - 1; i >= 0; --i)
+            //CoroutineRunner.StartCoroutine(CheckAtlasLifetimeWork());
+        }
+        public static IEnumerator CheckAtlasLifetimeWork()
+        {
+            yield return Resources.UnloadUnusedAssets();
+            yield return null;
+            List<Pack<string, WeakReference>> old = new List<Pack<string, WeakReference>>();
+            for (int i = 0; i < _LifetimeHolder.Count; ++i)
             {
-                var info = _LifetimeHolder[i];
-                if (!info.t1.GetWeakReference<Sprite>())
+                var atlas = _LifetimeHolder[i];
+                if (atlas)
                 {
-                    _LifetimeHolder.RemoveAt(i);
+                    old.Add(new Pack<string, WeakReference>(atlas.tag, new WeakReference(atlas)));
                 }
             }
-            yield return Resources.UnloadUnusedAssets();
+            _LifetimeHolder.Clear();
+            try
+            {
+                yield return Resources.UnloadUnusedAssets();
+            }
+            finally
+            {
+                for (int i = 0; i < old.Count; ++i)
+                {
+                    var info = old[i];
+                    var atlas = info.t2.GetWeakReference<SpriteAtlas>();
+                    if (atlas)
+                    {
+                        _LifetimeHolder.Add(atlas);
+                    }
+                    else
+                    {
+                        PlatDependant.LogError($"Atlas released. {info.t1}");
+                    }
+                }
+            }
         }
     }
 }
