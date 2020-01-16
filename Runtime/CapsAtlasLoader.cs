1cd8ed0f9b880b8d3b368f971661a35a0d54c125
Silas <hdss3609@gmail.com>
2020-01-16 16:00:23 +0800

    去掉暂时的 Atlas 丢失修正

--- Runtime/CapsAtlasLoader.cs
+++ Runtime/CapsAtlasLoader.cs
@@ -16,11 +16,6 @@ namespace Capstones.UnityEngineEx
         // In current version of UGUI, the Image.RebuildImage will crash when the image's active sprite is set to null before the atlas is loaded.
         // So we get this list using reflection, and do the filter by ourselves.
         private static List<Image> TrackedImages;
-        // We find that, sometimes, atlas will be unloaded while it is actually in use.
-        // This may be because that, an auto garbage collect is triggered after the scene is loaded.
-        // while the scene objs are going to refer atlas, but the refer has not been made, as the garbage collect happens too early.
-        // TODO: common lifetime holder.
-        private static List<SpriteAtlas> _LifetimeHolder = new List<SpriteAtlas>();
 
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         private static void OnUnityStart()
@@ -32,8 +27,6 @@ namespace Capstones.UnityEngineEx
                 // TODO: when we change the ResLoader dynamically (from EditorResLoader to ClientResLoader), we should call this again.
                 // or (from ClientResLoader to EditorResLoader) we should unregister SpriteAtlasManager.atlasRequested.
                 SpriteAtlasManager.atlasRequested += LoadAtlas;
-
-                UnityEngine.SceneManagement.SceneManager.sceneLoaded += (scene, mode) => CheckAtlasLifetime();
             }
         }
 
@@ -51,48 +44,7 @@ namespace Capstones.UnityEngineEx
                         TrackedImages.RemoveAt(i);
                     }
                 }
-                _LifetimeHolder.Add(atlas);
                 funcReg(atlas);
-            }
-        }
-
-        private static void CheckAtlasLifetime()
-        {
-            //CoroutineRunner.StartCoroutine(CheckAtlasLifetimeWork());
-        }
-        public static IEnumerator CheckAtlasLifetimeWork()
-        {
-            yield return Resources.UnloadUnusedAssets();
-            yield return null;
-            List<Pack<string, WeakReference>> old = new List<Pack<string, WeakReference>>();
-            for (int i = 0; i < _LifetimeHolder.Count; ++i)
-            {
-                var atlas = _LifetimeHolder[i];
-                if (atlas)
-                {
-                    old.Add(new Pack<string, WeakReference>(atlas.tag, new WeakReference(atlas)));
-                }
-            }
-            _LifetimeHolder.Clear();
-            try
-            {
-                yield return Resources.UnloadUnusedAssets();
-            }
-            finally
-            {
-                for (int i = 0; i < old.Count; ++i)
-                {
-                    var info = old[i];
-                    var atlas = info.t2.GetWeakReference<SpriteAtlas>();
-                    if (atlas)
-                    {
-                        _LifetimeHolder.Add(atlas);
-                    }
-                    else
-                    {
-                        PlatDependant.LogError($"Atlas released. {info.t1}");
-                    }
-                }
             }
         }
     }
